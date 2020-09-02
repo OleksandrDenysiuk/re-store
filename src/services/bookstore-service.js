7d42f710c632d37eab2f5ce8dc1ae84212e0157e
Oleksandr <denysiuk.oleksandr1997@gmail.com>
2020-09-02 10:31:16 +0200

    Add error handling

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -17,9 +17,14 @@ export default class BookstoreService {
     ];
 
     getBooks() {
-        return new Promise((resolve) => {
+        return new Promise((resolve, reject) => {
             setTimeout(() => {
-                resolve(this.data);
+                if(Math.random() > 0.75){
+                    reject(new Error('ERROR'));
+                }else{
+                    resolve(this.data);
+                }
+
             }, 700);
         });
     }
