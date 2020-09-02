2368f16b2dd3ffc35d9f107e6154c065c4c57056
Oleksandr <denysiuk.oleksandr1997@gmail.com>
2020-09-02 08:33:50 +0200

    Add styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,18 +2,22 @@ import React from "react";
 import {Route, Switch} from 'react-router-dom';
 import './app.css';
 import {CartPage, HomePage} from "../pages";
+import ShopHeader from "../shop-header";
 
 const App = () => {
     return (
-        <Switch>
-            <Route
-                path="/"
-                component={HomePage}
-                exact />
-            <Route
-                path="/"
-                component={CartPage} />
-        </Switch>
+        <main role="main" className="container">
+            <ShopHeader numItems={5} total={210} />
+            <Switch>
+                <Route
+                    path="/"
+                    component={HomePage}
+                    exact/>
+                <Route
+                    path="/"
+                    component={CartPage}/>
+            </Switch>
+        </main>
     )
 };
 
