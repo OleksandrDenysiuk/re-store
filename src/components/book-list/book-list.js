9c34a472f6c26e293927eac2fcd8e33d705b1a2a
Oleksandr <denysiuk.oleksandr1997@gmail.com>
2020-09-02 08:46:43 +0200

    Add promise to service, fix bags

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -6,19 +6,24 @@ import {withBookstoreService} from '../hoc';
 import {booksLoaded} from "../../actions";
 import {compose} from '../../utils';
 import './book-list.css';
+import Spinner from "../spinner";
 
 
 class BookList extends Component {
 
     componentDidMount() {
-        const {bookstoreService} = this.props;
-        const data = bookstoreService.getBooks();
-
-        this.props.booksLoaded(data);
+        const {bookstoreService, booksLoaded} = this.props;
+        bookstoreService.getBooks()
+            .then((data) => {
+                booksLoaded(data);
+            });
     }
 
     render() {
-        const { books } = this.props;
+        const { books, loading } = this.props;
+        if(loading){
+            return <Spinner />
+        }
         return (
             <ul className="book-list">
                 {
@@ -33,8 +38,8 @@ class BookList extends Component {
     }
 }
 
-const mapStateToProps = ({books}) => {
-    return {books};
+const mapStateToProps = ({books , loading}) => {
+    return {books, loading};
 };
 
 const mapDispatchToProps = {
--- src/components/shopping-cart-table/shopping-cart-table.js
+++ src/components/shopping-cart-table/shopping-cart-table.js
@@ -7,13 +7,14 @@ const ShoppingCartTable = () => {
             <h2>Your Order</h2>
             <table className="table">
                 <thead>
-                <th>#</th>
-                <th>Item</th>
-                <th>Count</th>
-                <th>Price</th>
-                <th>Action</th>
+                <tr>
+                    <th>#</th>
+                    <th>Item</th>
+                    <th>Count</th>
+                    <th>Price</th>
+                    <th>Action</th>
+                </tr>
                 </thead>
-
                 <tbody>
                 <tr>
                     <td>1</td>
@@ -22,13 +23,13 @@ const ShoppingCartTable = () => {
                     <td>$40</td>
                     <td>
                         <button className="btn btn-outline-danger btn-sm float-right">
-                            <i className="fa fa-trash-o" />
+                            <i className="fa fa-trash-o"/>
                         </button>
                         <button className="btn btn-outline-success btn-sm float-right">
-                            <i className="fa fa-plus-circle" />
+                            <i className="fa fa-plus-circle"/>
                         </button>
                         <button className="btn btn-outline-warning btn-sm float-right">
-                            <i className="fa fa-minus-circle" />
+                            <i className="fa fa-minus-circle"/>
                         </button>
                     </td>
                 </tr>
